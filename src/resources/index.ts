0af640d0de99c0cf117148170cc1773a4c0d5032
Santiago Acosta Meza <73088662+saacostam@users.noreply.github.com>
2023-11-26 03:02:44 -0500

    test asset reference

--- src/resources/index.ts
+++ src/resources/index.ts
@@ -1,31 +1,31 @@
 import {Color, ImageSource, Sound, Loader} from "excalibur";
 
 const Images = {
-    water: new ImageSource('/water.png'),
-    player: new ImageSource('/player.png'),
-    tree: new ImageSource('/tree.png'),
-    leaf: new ImageSource('/leafs.png'),
-    rock: new ImageSource('/rocks.png'),
-    flower: new ImageSource('/flower.png'),
-    grass: new ImageSource('/grass.png'),
-    bushBlocker: new ImageSource('/bush-block.png'),
-    waterProp: new ImageSource('/water-props.png'),
-    forestProp: new ImageSource('/forest-props.png'),
-    smallRock: new ImageSource('/small-rocks.png'),
-    bear: new ImageSource('/bear.png'),
-    deer: new ImageSource('/deer.png'),
-    camera: new ImageSource('/camera.png'),
-    frog: new ImageSource('/frog.png'),
-    froggy: new ImageSource('/froggy.png'),
-    condor: new ImageSource('/condor.png'),
+    water: new ImageSource('https://github.com/saacostam/tic-final-game/raw/main/docs/water.png'),
+    player: new ImageSource('https://github.com/saacostam/tic-final-game/raw/main/docs/player.png'),
+    tree: new ImageSource('https://github.com/saacostam/tic-final-game/raw/main/docs/tree.png'),
+    leaf: new ImageSource('https://github.com/saacostam/tic-final-game/raw/main/docs/leafs.png'),
+    rock: new ImageSource('https://github.com/saacostam/tic-final-game/raw/main/docs/rocks.png'),
+    flower: new ImageSource('https://github.com/saacostam/tic-final-game/raw/main/docs/flower.png'),
+    grass: new ImageSource('https://github.com/saacostam/tic-final-game/raw/main/docs/grass.png'),
+    bushBlocker: new ImageSource('https://github.com/saacostam/tic-final-game/raw/main/docs/bush-block.png'),
+    waterProp: new ImageSource('https://github.com/saacostam/tic-final-game/raw/main/docs/water-props.png'),
+    forestProp: new ImageSource('https://github.com/saacostam/tic-final-game/raw/main/docs/forest-props.png'),
+    smallRock: new ImageSource('https://github.com/saacostam/tic-final-game/raw/main/docs/small-rocks.png'),
+    bear: new ImageSource('https://github.com/saacostam/tic-final-game/raw/main/docs/bear.png'),
+    deer: new ImageSource('https://github.com/saacostam/tic-final-game/raw/main/docs/deer.png'),
+    camera: new ImageSource('https://github.com/saacostam/tic-final-game/raw/main/docs/camera.png'),
+    frog: new ImageSource('https://github.com/saacostam/tic-final-game/raw/main/docs/frog.png'),
+    froggy: new ImageSource('https://github.com/saacostam/tic-final-game/raw/main/docs/froggy.png'),
+    condor: new ImageSource('https://github.com/saacostam/tic-final-game/raw/main/docs/condor.png'),
 }
 
 const Sounds = {
-    nope: new Sound('/audio/nope.mp3'),
-    plop: new Sound('/audio/plop.mp3'),
-    yeah: new Sound('/audio/yeah.mp3'),
-    shutter: new Sound('/audio/camera.mp3'),
-    music: new Sound('/audio/music.mp3'),
+    nope: new Sound('https://github.com/saacostam/tic-final-game/raw/main/docs/audio/nope.mp3'),
+    plop: new Sound('https://github.com/saacostam/tic-final-game/raw/main/docs/audio/plop.mp3'),
+    yeah: new Sound('https://github.com/saacostam/tic-final-game/raw/main/docs/audio/yeah.mp3'),
+    shutter: new Sound('https://github.com/saacostam/tic-final-game/raw/main/docs/audio/camera.mp3'),
+    music: new Sound('https://github.com/saacostam/tic-final-game/raw/main/docs/audio/music.mp3'),
 }
 
 const loader = new Loader();

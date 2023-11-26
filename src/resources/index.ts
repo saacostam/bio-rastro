f4d7bb14bb96bc7cf5e01b96f942f260847e69be
Santiago Acosta Meza <73088662+saacostam@users.noreply.github.com>
2023-11-26 03:11:20 -0500

    Create LOCAL_ASSETS_MODE wrapper function

--- src/resources/index.ts
+++ src/resources/index.ts
@@ -1,31 +1,41 @@
 import {Color, ImageSource, Sound, Loader} from "excalibur";
 
+const LOCAL_ASSETS_MODE = false;
+
+function resolveFilePath(url: string){
+    return `${
+        LOCAL_ASSETS_MODE ?
+            '':
+            'https://raw.githubusercontent.com/saacostam/tic-final-game/main/docs'
+    }${url}`;
+}
+
 const Images = {
-    water: new ImageSource('https://github.com/saacostam/tic-final-game/raw/main/docs/water.png'),
-    player: new ImageSource('https://github.com/saacostam/tic-final-game/raw/main/docs/player.png'),
-    tree: new ImageSource('https://github.com/saacostam/tic-final-game/raw/main/docs/tree.png'),
-    leaf: new ImageSource('https://github.com/saacostam/tic-final-game/raw/main/docs/leafs.png'),
-    rock: new ImageSource('https://github.com/saacostam/tic-final-game/raw/main/docs/rocks.png'),
-    flower: new ImageSource('https://github.com/saacostam/tic-final-game/raw/main/docs/flower.png'),
-    grass: new ImageSource('https://github.com/saacostam/tic-final-game/raw/main/docs/grass.png'),
-    bushBlocker: new ImageSource('https://github.com/saacostam/tic-final-game/raw/main/docs/bush-block.png'),
-    waterProp: new ImageSource('https://github.com/saacostam/tic-final-game/raw/main/docs/water-props.png'),
-    forestProp: new ImageSource('https://github.com/saacostam/tic-final-game/raw/main/docs/forest-props.png'),
-    smallRock: new ImageSource('https://github.com/saacostam/tic-final-game/raw/main/docs/small-rocks.png'),
-    bear: new ImageSource('https://github.com/saacostam/tic-final-game/raw/main/docs/bear.png'),
-    deer: new ImageSource('https://github.com/saacostam/tic-final-game/raw/main/docs/deer.png'),
-    camera: new ImageSource('https://github.com/saacostam/tic-final-game/raw/main/docs/camera.png'),
-    frog: new ImageSource('https://github.com/saacostam/tic-final-game/raw/main/docs/frog.png'),
-    froggy: new ImageSource('https://github.com/saacostam/tic-final-game/raw/main/docs/froggy.png'),
-    condor: new ImageSource('https://github.com/saacostam/tic-final-game/raw/main/docs/condor.png'),
+    water: new ImageSource(resolveFilePath('/water.png')),
+    player: new ImageSource(resolveFilePath('/player.png')),
+    tree: new ImageSource(resolveFilePath('/tree.png')),
+    leaf: new ImageSource(resolveFilePath('/leafs.png')),
+    rock: new ImageSource(resolveFilePath('/rocks.png')),
+    flower: new ImageSource(resolveFilePath('/flower.png')),
+    grass: new ImageSource(resolveFilePath('/grass.png')),
+    bushBlocker: new ImageSource(resolveFilePath('/bush-block.png')),
+    waterProp: new ImageSource(resolveFilePath('/water-props.png')),
+    forestProp: new ImageSource(resolveFilePath('/forest-props.png')),
+    smallRock: new ImageSource(resolveFilePath('/small-rocks.png')),
+    bear: new ImageSource(resolveFilePath('/bear.png')),
+    deer: new ImageSource(resolveFilePath('/deer.png')),
+    camera: new ImageSource(resolveFilePath('/camera.png')),
+    frog: new ImageSource(resolveFilePath('/frog.png')),
+    froggy: new ImageSource(resolveFilePath('/froggy.png')),
+    condor: new ImageSource(resolveFilePath('/condor.png')),
 }
 
 const Sounds = {
-    nope: new Sound('https://github.com/saacostam/tic-final-game/raw/main/docs/audio/nope.mp3'),
-    plop: new Sound('https://github.com/saacostam/tic-final-game/raw/main/docs/audio/plop.mp3'),
-    yeah: new Sound('https://github.com/saacostam/tic-final-game/raw/main/docs/audio/yeah.mp3'),
-    shutter: new Sound('https://github.com/saacostam/tic-final-game/raw/main/docs/audio/camera.mp3'),
-    music: new Sound('https://github.com/saacostam/tic-final-game/raw/main/docs/audio/music.mp3'),
+    nope: new Sound(resolveFilePath('/audio/nope.mp3')),
+    plop: new Sound(resolveFilePath('/audio/plop.mp3')),
+    yeah: new Sound(resolveFilePath('/audio/yeah.mp3')),
+    shutter: new Sound(resolveFilePath('/audio/camera.mp3')),
+    music: new Sound(resolveFilePath('/audio/music.mp3')),
 }
 
 const loader = new Loader();
